8a001807adab97868f09278bca521124561b7b8c
Ubuntu <ubuntu@ip-172-31-36-79.us-east-2.compute.internal>
2020-03-27 17:59:45 +0000

    Add tests

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,6 +1,20 @@
 class HerosController < ApplicationController
   def index
-    @heros = Hero.all
-    render json: @heros
+    heros = Hero.all
+    render json: heros
+  end
+  
+  def create
+    # Create a new cat
+    hero = Hero.create(hero_params)
+
+    # respond with our new cat
+    render json: hero
+  end
+
+  # Handle strong parameters, so we are secure
+  private
+  def hero_params
+    params.require(:hero).permit(:name, :age, :super_power, :description)
   end
 end
--- spec/requests/heros_request_spec.rb
+++ spec/requests/heros_request_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Heros", type: :request do
+  it "gets a list of Heros" do
+    # Create a new cat in the Test Database (not the same one as development)
+    Hero.create(name: 'Spiderman', age: 17, super_power: 'spidey senses', description: 'Walks in the park')
 
+    # Make a request to the API
+    get '/heros'
+
+    # Convert the response into a Ruby Hash
+    json = JSON.parse(response.body)
+
+    # Assure that we got a successful response
+    expect(response).to have_http_status(:ok)
+
+    # Assure that we got one result back as expected
+    expect(json.length).to eq 1
+  end
 end
+
